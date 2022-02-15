ea4fc9bef46219a2bbcf62b4746f873af1c44a89
fiigmnt <fiigmnt@protonmail.com>
2022-02-15 10:16:15 -0500

    init

---
+++ README.md
@@ -0,0 +1,33 @@
+# Protein Onboarding Bot 🤖
+
+>it's probably nothing
+
+## TL;DR
+
+A custom built bot for Protein to assist with onboarding new members.  ✨
+
+## How it Works
+
+1. Somone submits a request to join the community
+2. The request is posted in `member-applications`
+3. Community members respond to the request via: 🟢 or 🔴
+4. The response is recorded in airtable for community managers
+
+## Setup
+
+1. Fork the repository to your gihub account (top right hand corner)
+2. Clone the fork to your local system
+3. Install Node modules `npm install`
+
+## Submitting your improvements
+
+1. Create a new branch `git check -b <your branch name>`
+2. `git add .`
+3. `git commit -m 'fix(example): semantic versioning is cool'`
+4. `git push`
+5. Grab that PR and point it to our main branch
+6. Message with changes for a review ! :v:
+
+## Support
+
+Feel free to reach out to me `fiigmnt#0001` on discord, or email: `fiigmnt@pm.me`  [#dev1462](https://twitter.com/fiigmnt) 🤙 https://discord.js.org/)
---
+++ source/index.ts
@@ -0,0 +1,142 @@
+// ----------------------------------------------------------------------------------//
+// RADAR Portal
+// Whitelist onboarding bot (( BETA v0.1.0 ))
+// Fiigmnt | Febuary 9, 2022 | Updated: Febuary 14, 2022
+// ----------------------------------------------------------------------------------//
+
+import {
+  Client,
+  Intents,
+  MessageActionRow,
+  MessageButton,
+  MessageEmbed,
+  Interaction,
+} from "discord.js";
+import Airtable from "airtable";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const { DISCORD_TOKEN, AIRTABLE_TOKEN, AIRTABLE_TABLE_KEY, ROLE_ID } = process.env;
+
+const base = new Airtable({ apiKey: AIRTABLE_TOKEN }).base(
+  AIRTABLE_TABLE_KEY || ""
+);
+const table = base("Table 1");
+
+const client = new Client({
+  intents: [
+    Intents.FLAGS.GUILDS,
+    Intents.FLAGS.GUILD_MESSAGES,
+    Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+  ],
+  partials: ["MESSAGE", "CHANNEL", "REACTION"],
+});
+
+client.once("ready", () => {
+  console.log("Bot is ready");
+});
+
+const isUserAccepted = async (interaction: Interaction) => {
+  const username = `${interaction.user.username}#${interaction.user.discriminator}`;
+
+  console.log("-- USER --");
+  console.log(username);
+
+  try {
+    // // find record in airtable with the email
+    const result = await table
+      .select({
+        filterByFormula: `({Discord (don’t forget to include the # number. E.g Futurist#1234)} = "${username}")`,
+      })
+      .firstPage();
+
+    return !!result.length;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// on bot being added to server
+client.on("guildCreate", async (guild) => {
+  try {
+    console.log(
+      `New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`
+    );
+
+    // create connect channel
+    const connectChannel = await guild.channels.create("radar-portal", {
+      type: "GUILD_TEXT",
+    });
+
+    if (connectChannel?.id) {
+      // create button in channel
+      const joinButton = new MessageActionRow().addComponents(
+        new MessageButton()
+          .setCustomId("join")
+          .setLabel("Join")
+          .setStyle("PRIMARY")
+      );
+
+      const joinInfo = new MessageEmbed()
+        .setTitle("Wecome to RADAR")
+        .setDescription("Click to see if you're in on the list");
+
+      await connectChannel.send({
+        embeds: [joinInfo],
+        components: [joinButton],
+      });
+    } else {
+      throw new Error("Couldn't create channel");
+    }
+  } catch (error: any) {
+    throw new Error(error);
+  }
+});
+
+// read button interactions
+client.on("interactionCreate", async (interaction) => {
+  if (!interaction.isButton()) return;
+
+  try {
+    // check to see if they're in the waitlist
+    if (
+      interaction.customId === "join" &&
+      (await isUserAccepted(interaction))
+    ) {
+      let role = interaction.guild?.roles.cache.find(
+        (r) => r.id === ROLE_ID
+      );
+
+      if (role) {
+        const member = interaction.guild?.members.cache.find(
+          (member) => member.id === interaction.user.id
+        );
+
+        if (member) member.roles.add(role);
+      }
+
+      await interaction.reply({
+        content: `Welcome to the future of futures ${interaction.user.username}`,
+        ephemeral: true,
+      });
+    } else {
+      const denyButton = new MessageActionRow().addComponents(
+        new MessageButton()
+          .setLabel("Make sure you’ve applied here")
+          .setStyle("LINK")
+          .setURL("https://airtable.com/shrFKUzeNpJoDU0x9")
+      );
+      await interaction.reply({
+        content:
+          "Sorry, we can’t find you on the list.\nIs this an error? Reach out via the #help channel.",
+        ephemeral: true,
+        components: [denyButton],
+      });
+    }
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+client.login(DISCORD_TOKEN);
